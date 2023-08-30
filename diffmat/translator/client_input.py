91d21d45bec2752eaa8a678f882715e63b57b7d4
Michael Birsak <birsakm@kw61091.kaust.edu.sa>
2023-08-30 15:40:25 +0300

    Added cache functionality to client_input.py
    Now the SAT server is only contacted in case the necessary external input files are not already available.

--- diffmat/translator/client_input.py
+++ diffmat/translator/client_input.py
@@ -10,6 +10,7 @@ import socket
 import os
 
 SIZE = 1024
+IMG_TEXT_FILE_NAME = 'img_file_names.txt'
 
 def receive_file_binary(conn):
     expected_size = b""
@@ -43,23 +44,45 @@ def send_file_binary(img_file_path, socket):
     socket.sendall(raw)
 
 class ClientInputGenerator(ExtInputGenerator):
-    def __init__(self, root: ET.Element, res: int, host: str, port: int):
+    def __init__(self, root: ET.Element, res: int, host: str, port: int, keep_connected: bool = False):
         
         self.host = host
         self.port = port
+        self.keep_connected = keep_connected
+        self.is_connected = False
         
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.host, self.port))
-        print('INFO: Connected to SAT Server (Host: {}, Port: {})'.format(self.host, self.port))
+        if self.keep_connected:
+            self.connect()
         
         super().__init__(root = root, res = res)
+    
+    def connect(self):
+        if not self.is_connected:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.host, self.port))
+            self.is_connected = True
+        # print('INFO: Connected to SAT Server (Host: {}, Port: {})'.format(self.host, self.port))
         
+    def disconnect(self):
+        if self.is_connected:
+            self.client_socket.close()
+            self.is_connected = False
+            
+    def connected(self):
+        return self.is_connected
+    
     def detect_sat_version(self):
         # Contact Server to ask for SAT version
         command = "sbscooker --version"
         
+        if not self.keep_connected:
+            self.connect()
+        
         self.client_socket.sendall(command.encode())
         output = self.client_socket.recv(SIZE).decode()
+        
+        if not self.keep_connected:
+            self.disconnect()
         
         version_tokens = str.split(output, ' ')
 
@@ -81,11 +104,24 @@ class ClientInputGenerator(ExtInputGenerator):
         send_file_binary(file_path, self.client_socket)
         
     def run_sbscooker(self, file_path):
+        if not self.connected():
+            self.connect()
+        
+        self.send_file(file_path)
+        
         file_name = os.path.split(file_path)[1]
         self.client_socket.sendall("sbscooker {}".format(file_name).encode())
-        return self.client_socket.recv(SIZE).decode()
+        output = self.client_socket.recv(SIZE).decode()
+        
+        if not self.keep_connected:
+            self.disconnect()
+        
+        return output
     
     def run_sbsrender(self, file_path, output_path):
+        if not self.connected():
+            self.connect()
+        
         os.makedirs(output_path, exist_ok = True)
         file_name = os.path.split(file_path)[1]
         self.client_socket.sendall("sbsrender {}".format(file_name).encode())
@@ -96,20 +132,31 @@ class ClientInputGenerator(ExtInputGenerator):
         
         self.client_socket.send('ACK'.encode())
         
-        for i in range(num_files):
-            file_name = self.client_socket.recv(SIZE).decode()
-            self.client_socket.send('ACK'.encode())
-            
-            img_file_path = os.path.join(output_path, file_name)
-            
-            packet = receive_file_binary(self.client_socket)
+        text_file_path = os.path.join(output_path, IMG_TEXT_FILE_NAME)
+        
+        with open(text_file_path, 'w') as img_f:
+            for i in range(num_files):
+                file_name = self.client_socket.recv(SIZE).decode()
+                self.client_socket.send('ACK'.encode())
                 
-            with open(img_file_path, 'wb') as f:
-                f.write(packet)
+                img_file_path = os.path.join(output_path, file_name)
                 
-            self.client_socket.send('ACK'.encode())
+                packet = receive_file_binary(self.client_socket)
+                    
+                with open(img_file_path, 'wb') as f:
+                    f.write(packet)
+                    
+                self.client_socket.send('ACK'.encode())
             
-        return 'OK'        
+                img_f.write(file_name)
+                
+                if i + 1 < num_files:
+                    img_f.write('\n')
+                    
+        if not self.keep_connected:
+            self.disconnect()
+            
+        return 'OK'
     
     def _run_sat_command(self, command: str) -> int:
         """Executes an SAT command and detect warnings/errors reported by the program.
@@ -126,16 +173,21 @@ class ClientInputGenerator(ExtInputGenerator):
         
         command_tokens = str.split(command, ' ')
         
+        output = 'OK'
         if command_tokens[0] == '"sbscooker"':
             sbs_file_path = command_tokens[1][1:-1]
             assert(str.endswith(sbs_file_path, '.sbs'))
             
-            self.send_file(sbs_file_path)
-            output = self.run_sbscooker(sbs_file_path)
+            if not os.path.isfile(os.path.join(os.path.split(sbs_file_path)[0], IMG_TEXT_FILE_NAME)):
+                # self.send_file(sbs_file_path) # This was moved to run_sbscooker
+                output = self.run_sbscooker(sbs_file_path)
+            
         elif command_tokens[0] == '"sbsrender"':
             sbs_file_path = command_tokens[2][1:-1]
             assert(str.endswith(sbs_file_path, '.sbsar'))
-            output = self.run_sbsrender(sbs_file_path, command_tokens[-1][1:-1])
+            
+            if not os.path.isfile(os.path.join(os.path.split(sbs_file_path)[0], IMG_TEXT_FILE_NAME)):
+                output = self.run_sbsrender(sbs_file_path, command_tokens[-1][1:-1])
         
         # output = subprocess.run(command, shell=True, capture_output=True, text=True)
         ret_code = 0
